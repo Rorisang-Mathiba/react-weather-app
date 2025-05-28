f02dcf6523949bbf6ec110712adc2af8a8c1abbc
Rorisang-Mathiba <rmathiba71@gmail.com>
2025-05-28 17:31:07 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
-
+import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-
+      <Weather />
       <footer>
         This project was created by{" "}
         <a
